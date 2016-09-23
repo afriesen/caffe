bfe2246bcef7010cf605b08a5089039996f160fb
Abe Friesen <afriesen@gmail.com>
2016-09-22 23:09:35 -0700

    Fixed interp to build under CPU_ONLY mode. Fixed up sspn proto to build properly.

--- include/caffe/util/interp.hpp
+++ include/caffe/util/interp.hpp
@@ -3,7 +3,9 @@
 #ifndef CAFFE_UTIL_INTERP_H_
 #define CAFFE_UTIL_INTERP_H_
 
+#ifndef CPU_ONLY
 #include <cublas_v2.h>
+#endif // CPU_ONLY
 #include "caffe/proto/caffe.pb.h"
 
 namespace caffe {
@@ -17,10 +19,12 @@ void caffe_cpu_interp2(const int channels,
     const Dtype *data1, const int x1, const int y1, const int height1, const int width1, const int Height1, const int Width1,
           Dtype *data2, const int x2, const int y2, const int height2, const int width2, const int Height2, const int Width2);
 
+#ifndef CPU_ONLY
 template <typename Dtype, bool packed>
 void caffe_gpu_interp2(const int channels,
     const Dtype *data1, const int x1, const int y1, const int height1, const int width1, const int Height1, const int Width1,
           Dtype *data2, const int x2, const int y2, const int height2, const int width2, const int Height2, const int Width2);
+#endif // CPU_ONLY
 
 // Backward (adjoint) operation
 template <typename Dtype, bool packed>
@@ -28,10 +32,12 @@ void caffe_cpu_interp2_backward(const int channels,
 	  Dtype *data1, const int x1, const int y1, const int height1, const int width1, const int Height1, const int Width1,
     const Dtype *data2, const int x2, const int y2, const int height2, const int width2, const int Height2, const int Width2);
 
+#ifndef CPU_ONLY
 template <typename Dtype, bool packed>
 void caffe_gpu_interp2_backward(const int channels,
 	  Dtype *data1, const int x1, const int y1, const int height1, const int width1, const int Height1, const int Width1,
     const Dtype *data2, const int x2, const int y2, const int height2, const int width2, const int Height2, const int Width2);
+#endif // CPU_ONLY
 
 // Create Gaussian pyramid of an image. Assume output space is pre-allocated.
 // IN : [channels height width]
@@ -40,10 +46,12 @@ void caffe_cpu_pyramid2(const int channels,
     const Dtype *data, const int height, const int width,
     Dtype *data_pyr, const int levels);
 
+#ifndef CPU_ONLY
 template <typename Dtype, bool packed>
 void caffe_gpu_pyramid2(const int channels,
     const Dtype *data, const int height, const int width,
     Dtype *data_pyr, const int levels);
+#endif // CPU_ONLY
 
   /*
 template <typename Dtype, bool packed>
