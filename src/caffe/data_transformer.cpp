b6c5d4cdeaf2fae1a4f5c38fd664a395524c74c3
Abe <afriesen@gmail.com>
2016-09-23 10:18:33 -0700

    Fixed bug in check for data transformer.

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -730,7 +730,7 @@ vector<int> DataTransformer<Dtype>::InferBlobShape(
 template <typename Dtype>
 void DataTransformer<Dtype>::InitRand() {
   const bool needs_rand = param_.mirror() ||
-      (phase_ == TRAIN && param_.crop_size());
+      (phase_ == TRAIN && (param_.crop_size() || param_.crop_height() || param_.crop_width()));
   if (needs_rand) {
     const unsigned int rng_seed = caffe_rng_rand();
     rng_.reset(new Caffe::RNG(rng_seed));
